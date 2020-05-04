bf601123e922cb820a918e5965b87d4c0a827675
Victor <pvictor@protonmail.com>
2020-05-04 18:12:32 +0200

    Update siteConfig.js

--- siteConfig.js
+++ siteConfig.js
@@ -1,24 +1,24 @@
 module.exports = {
     // Do not include a trailing slash!
-    siteUrl: `https://styxlab.github.io`,
+    siteUrl: `https://madebyvertex.co`,
 
     // Maximum number of post shown per page
     // With Infinite Scroll this is the chunk of posts fetched when scrolling down
     postsPerPage: 3,
 
     // This allows an alternative site title for meta data for pages.
-    siteTitleMeta: `Gatsby Starter Ghost CMS`,
+    siteTitleMeta: `Vertex`,
 
     // This allows an site description for meta data for pages.
-    siteDescriptionMeta: `Turn your Ghost blog into a flaring fast static site with Gatsby`,
+    siteDescriptionMeta: `Association loi 1901 ayant pour mission de répandre un usage vertueux de la technologie.`,
 
     // Change to the width and height of your default share image
     shareImageWidth: 1000,
     shareImageHeight: 523,
 
     // Used for App and Offline manifest e.g. Mobile Home Screen
-    shortTitle: `Jamify`,
+    shortTitle: `Vertex`,
     siteIcon: `favicon.png`,
-    backgroundColor: `#e9e9e9`,
-    themeColor: `#15171A`,
+    backgroundColor: `#fffff`,
+    themeColor: `#61c8ff`,
 }
